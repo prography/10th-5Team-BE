0411bad45d1beebaae02dff9326471efca164090
GoToBILL <wnqudwnrlrl@naver.com>
2025-05-26 15:58:44 +0900

    feat: spring 초기 세팅

---
+++ src/main/java/com/example/cherrydan/CherrydanApplication.java
@@ -0,0 +1,13 @@
+package com.example.cherrydan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherrydanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherrydanApplication.class, args);
+    }
+
+}
