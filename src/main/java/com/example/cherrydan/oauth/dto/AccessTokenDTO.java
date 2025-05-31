accfb77ec8e80035147bd27c2a9ddd411112bc06
GoToBILL <wnqudwnrlrl@naver.com>
2025-06-01 00:23:22 +0900

    feat: apple auth add

---
+++ src/main/java/com/example/cherrydan/oauth/dto/AccessTokenDTO.java
@@ -0,0 +1,16 @@
+package com.example.cherrydan.oauth.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "액세스 토큰 정보 (토큰 갱신용)")
+public class AccessTokenDTO {
+    
+    @Schema(description = "액세스 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
+    private String accessToken;
+}
