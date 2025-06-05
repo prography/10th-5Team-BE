3f99101c684b3fe81443be66455ecc307768620e
GoToBILL <wnqudwnrlrl@naver.com>
2025-06-05 14:05:10 +0900

    refactor: baseTimeEntity 로직 수정

--- src/main/java/com/example/cherrydan/common/entity/BaseTimeEntity.java
+++ src/main/java/com/example/cherrydan/common/entity/BaseTimeEntity.java
@@ -1,17 +1,12 @@
 package com.example.cherrydan.common.entity;
 
-import com.example.cherrydan.common.audit.StringDateTimeProvider;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
@@ -25,15 +20,4 @@ public abstract class BaseTimeEntity {
     @LastModifiedDate
     @Column(name = "updated")
     private String updated;
-    
-    @PrePersist
-    public void onPrePersist() {
-        this.created = StringDateTimeProvider.now();
-        this.updated = this.created;
-    }
-    
-    @PreUpdate
-    public void onPreUpdate() {
-        this.updated = StringDateTimeProvider.now();
-    }
 }
