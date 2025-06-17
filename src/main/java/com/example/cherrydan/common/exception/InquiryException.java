bf68445176268d6c20b941d44c3b70f1f978df62
GoToBILL <wnqudwnrlrl@naver.com>
2025-06-17 19:53:44 +0900

    feat: appVersion, inquiry, notice Exception add

---
+++ src/main/java/com/example/cherrydan/common/exception/InquiryException.java
@@ -0,0 +1,7 @@
+package com.example.cherrydan.common.exception;
+
+public class InquiryException extends BaseException {
+    public InquiryException(ErrorMessage errorMessage) {
+        super(errorMessage);
+    }
+}
