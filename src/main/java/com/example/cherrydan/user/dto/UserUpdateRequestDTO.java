7a74bc1dac5decd4715f4f3b487f8f35516f653c
GoToBILL <wnqudwnrlrl@naver.com>
2025-06-19 19:09:13 +0900

    feat(user): UserUpdateRequestDTO 생성 (유저 정보 수정용 DTO)

---
+++ src/main/java/com/example/cherrydan/user/dto/UserUpdateRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.cherrydan.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateRequestDTO {
+    private String nickname;
+    private String mdn;
+} 
