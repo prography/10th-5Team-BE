003943613a2ae3b453fc04b4edd6dedb494de5e0
GoToBILL <wnqudwnrlrl@naver.com>
2025-06-19 18:22:14 +0900

    fix: add @Data annotation to InquiryRequestDTO for setter support

--- src/main/java/com/example/cherrydan/inquiry/dto/InquiryRequestDTO.java
+++ src/main/java/com/example/cherrydan/inquiry/dto/InquiryRequestDTO.java
@@ -2,10 +2,9 @@ package com.example.cherrydan.inquiry.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class InquiryRequestDTO {
