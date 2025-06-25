21320a47ecea8a8fb5b33dbe19e364820656e20c
o-ddong <odh0112@naver.com>
2025-06-26 02:47:34 +0900

    Docs: 내 체험단, 팝업 API Swagger 추가

--- src/main/java/com/example/cherrydan/campaign/dto/CampaignStatusPopupResponseDTO.java
+++ src/main/java/com/example/cherrydan/campaign/dto/CampaignStatusPopupResponseDTO.java
@@ -1,16 +1,25 @@
 package com.example.cherrydan.campaign.dto;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Getter;
-import java.util.List;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
 @Builder
+@Schema(description = "내 체험단 팝업용 상태 응답 DTO")
 public class CampaignStatusPopupResponseDTO {
+    @Schema(description = "신청 상태 총 개수", example = "4")
     private long applyTotal;
+    @Schema(description = "선정 상태 총 개수", example = "2")
     private long selectedTotal;
+    @Schema(description = "등록 상태 총 개수", example = "1")
     private long registeredTotal;
+    @Schema(description = "신청 상태 리스트")
     private List<CampaignStatusResponseDTO> apply;
+    @Schema(description = "선정 상태 리스트")
     private List<CampaignStatusResponseDTO> selected;
+    @Schema(description = "등록 상태 리스트")
     private List<CampaignStatusResponseDTO> registered;
-} 
+}
