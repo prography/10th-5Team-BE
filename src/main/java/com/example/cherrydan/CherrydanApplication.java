2cfafb05c74d1d987c730a4920dd776345000366
GoToBILL <wnqudwnrlrl@naver.com>
2025-07-10 23:39:38 +0900

    refactor: add @EnableScheduling

--- src/main/java/com/example/cherrydan/CherrydanApplication.java
+++ src/main/java/com/example/cherrydan/CherrydanApplication.java
@@ -2,8 +2,10 @@ package com.example.cherrydan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CherrydanApplication {
 
     public static void main(String[] args) {
