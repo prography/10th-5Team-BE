5d1a5463149c410c96070a41b32d3309d19f3eee
GoToBILL <wnqudwnrlrl@naver.com>
2025-08-01 13:41:35 +0900

    feat: monitoring yml & logback.xml

--- src/main/java/com/example/cherrydan/oauth/config/RestTemplateConfig.java
+++ src/main/java/com/example/cherrydan/oauth/config/RestTemplateConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.Profile;
 
-@Profile("prod")
+@Profile({"prod","local"})
 @Configuration
 public class RestTemplateConfig {
     @Bean
