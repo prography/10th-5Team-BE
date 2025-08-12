e1846bd761b987eb656e4a74f5af0f21a2ddcd7d
GoToBILL <wnqudwnrlrl@naver.com>
2025-08-12 15:07:39 +0900

    refactor: user entity 수정 -> 리프레쉬 토큰 연관관계 제거

--- src/main/java/com/example/cherrydan/user/domain/User.java
+++ src/main/java/com/example/cherrydan/user/domain/User.java
@@ -2,7 +2,6 @@ package com.example.cherrydan.user.domain;
 
 import com.example.cherrydan.common.entity.BaseTimeEntity;
 import com.example.cherrydan.oauth.model.AuthProvider;
-import com.example.cherrydan.oauth.model.RefreshToken;
 import com.example.cherrydan.utils.MaskingUtil;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -57,9 +56,6 @@ public class User extends BaseTimeEntity {
     // 프로필 이미지 URL
     private String picture;
 
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    @JoinColumn(name = "refresh_token_id")
-    private RefreshToken refreshToken;
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "push_settings_id")
