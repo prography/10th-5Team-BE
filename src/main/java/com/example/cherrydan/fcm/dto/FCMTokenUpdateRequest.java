cc02865805e454db689d4c4390b8f55c458f7134
GoToBILL <wnqudwnrlrl@naver.com>
2025-08-18 15:21:55 +0900

    feat: FCM 토큰 관리를 위한 DTO 클래스들을 추가했습니다

---
+++ src/main/java/com/example/cherrydan/fcm/dto/FCMTokenUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.example.cherrydan.fcm.dto;
+
+import lombok.*;
+
+/**
+ * FCM 토큰 수정 요청 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FCMTokenUpdateRequest {
+    private Long deviceId;
+    private String fcmToken;
+}
