8588192621cdd275377a42664ae352f0a2b0cb3f
GoToBILL <wnqudwnrlrl@naver.com>
2025-08-26 22:43:28 +0900

    refactor: OAuth2AuthenticationProcessingException 생성자 파라미터명을 errorMessage로 통일했습니다

--- src/main/java/com/example/cherrydan/oauth/security/oauth2/exception/OAuth2AuthenticationProcessingException.java
+++ src/main/java/com/example/cherrydan/oauth/security/oauth2/exception/OAuth2AuthenticationProcessingException.java
@@ -1,9 +1,17 @@
 package com.example.cherrydan.oauth.security.oauth2.exception;
 
+import com.example.cherrydan.common.exception.ErrorMessage;
 import org.springframework.security.core.AuthenticationException;
 
 public class OAuth2AuthenticationProcessingException extends AuthenticationException {
-    public OAuth2AuthenticationProcessingException(String msg) {
-        super(msg);
+    private final ErrorMessage errorMessage;
+
+    public OAuth2AuthenticationProcessingException(ErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
+        this.errorMessage = errorMessage;
+    }
+
+    public ErrorMessage getErrorMessage() {
+        return errorMessage;
     }
 }
