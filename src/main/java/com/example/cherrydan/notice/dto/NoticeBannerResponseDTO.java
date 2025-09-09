bb990e309e4dac96eb561cf8553c26d9f904ede8
o-ddong <odh0112@naver.com>
2025-09-09 23:54:33 +0900

    Feat: Swagger 누락된 import 추가

--- src/main/java/com/example/cherrydan/notice/dto/NoticeBannerResponseDTO.java
+++ src/main/java/com/example/cherrydan/notice/dto/NoticeBannerResponseDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
 @NoArgsConstructor
