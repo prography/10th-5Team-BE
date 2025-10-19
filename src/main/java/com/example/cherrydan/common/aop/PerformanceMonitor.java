5a49f7dd74c07160e61dfadedd5224dcab66024f
GoToBILL <wnqudwnrlrl@naver.com>
2025-10-19 15:58:36 +0900

    feat: 슬로우 쿼리 파악을 위한 ServiceMetricsAspect를 추가했습니다

--- src/main/java/com/example/cherrydan/common/aop/PerformanceMonitor.java
+++
@@ -1,9 +0,0 @@
-package com.example.cherrydan.common.aop;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface PerformanceMonitor {
-    String value() default "";
-} 
