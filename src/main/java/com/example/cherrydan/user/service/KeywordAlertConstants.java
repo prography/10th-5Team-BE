3098e5b7a6eadb22b514f7ca90b643b4db80530f
GoToBILL <wnqudwnrlrl@naver.com>
2025-11-30 11:40:26 +0900

    refactor: 푸시 메세지 상수화

---
+++ src/main/java/com/example/cherrydan/user/service/KeywordAlertConstants.java
@@ -0,0 +1,9 @@
+package com.example.cherrydan.user.service;
+
+public class KeywordAlertConstants {
+    public static final int THRESHOLD_HUNDRED = 100;
+    public static final int THRESHOLD_TEN = 10;
+    public static final String HUNDRED_PLUS = "100+";
+    public static final String TEN_PLUS = "10+";
+    public static final String messageTitle = "체리단";
+}
